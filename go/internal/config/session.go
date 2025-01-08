13247c61cf2fbbaaa68b83f0bb1dad6ddaac1fa7
Lim Swee Tat <st_lim@stlim.net>
2025-01-08 15:42:25 +0800

    Adding a config that can be used with every session - need to separate the global read from config file out from the per context config

--- go/internal/config/session.go
+++ go/internal/config/session.go
@@ -3,12 +3,18 @@ package config
 import "github.com/chromedp/chromedp"
 
 type SessionConfig struct {
-	// URL is the websocket URL to connect to the Chrome DevTools Protocol.
-	URL string `json:"url" mapstructure:"url"`
+	// ContextOptions are the options to pass to the context.
+	ContextOptions []chromedp.ContextOption `json:"context_options" mapstructure:"context_options"`
+
+	// ExecAllocatorOptions are the options to pass to the exec allocator.
+	ExecAllocatorOptions []chromedp.ExecAllocatorOption `json:"exec_allocator_options" mapstructure:"exec_allocator_options"`
 
 	// RemoteAllocatorOptions are the options to pass to the remote allocator.
 	RemoteAllocatorOptions []chromedp.RemoteAllocatorOption `json:"remote_allocator_options" mapstructure:"remote_allocator_options"`
 
-	// ContextOptions are the options to pass to the context.
-	ContextOptions []chromedp.ContextOption `json:"context_options" mapstructure:"context_options"`
+	// URL is the websocket URL to connect to the Chrome DevTools Protocol.
+	// The url with the following formats are accepted:
+	//   - ws://127.0.0.1:9222/
+	//   - http://127.0.0.1:9222/
+	URL string `json:"url" mapstructure:"url"`
 }
